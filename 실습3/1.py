c8c9209e918f44529d85a15ce91f21062920bed5
minseok1109 <park485201@naver.com>
2021-09-23 18:34:43 +0900

    토대만 작성

---
+++ 실습3/1.py
@@ -0,0 +1,21 @@
+class Arraylist:
+    def __init__(self):
+        self.items = []
+
+    def add(self, elem):
+    def remove(self, elem):
+    def insert(self,pos,elem):
+    def delete(self, pos):
+    def isEmpty(self):
+    def getEntry(self):
+    def size(self):
+    def clear(self):
+    def find(self):
+    def replace(self,pos,elem):
+    def sort(self):
+    def merge(self, lst):
+    def print(self, msg="ArrayList"):
+
+
+lst = []
+lst.append(1)
---
+++ 실습3/3.py
@@ -0,0 +1,14 @@
+class Set:
+    def __init__(self):
+        self.items = []
+
+    def __eq__(self, setB):
+    def insert(self, elem):
+    def delete(self, elem):
+    def union(self, setB):
+    def intersect(self, setB):
+    def difference(self, setB):
+    def isSubset(self,setB):
+    def isProperSubset(self, setB):
+    def size(self):
+    def print(self, msg):
